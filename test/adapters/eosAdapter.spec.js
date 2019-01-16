18d55d0d2fe057f5d6f36b4330de43c402d0ef80
devanshu <devanshudd@gmail.com>
2019-01-16 19:42:09 +0530

    included test dirs and removed unnecessary docker related files

---
+++ test/adapters/eosAdapter.spec.js
@@ -0,0 +1,8 @@
+import EOSAdapter from '../../src/adapters/eosAdapter';
+import chai from 'chai'; 
+import logger from '../../src/lib/logger';
+// import config from '../config.json';
+// import _ from 'lodash';
+
+describe('EOSAdapter Adapter', () => {
+})
